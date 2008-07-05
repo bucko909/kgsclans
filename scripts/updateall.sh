ade3eacb49ccbd7085b11d5a796eea72a7b6a7c1
David Buckley <kgsclans@bucko.me.uk>
2008-07-05 19:00:39 +0000

    clanperiod -> period_id

--- scripts/updateall.sh
+++ scripts/updateall.sh
@@ -6,10 +6,10 @@ cd /home/kgs
 mysql < scripts/makegrids.sql >> /home/kgs/UPDATE.OUT 2>> /home/kgs/UPDATE.ERR
 
 # Log clan points history
-mysql -se 'INSERT INTO clanpoints_history SELECT UNIX_TIMESTAMP(), id, points FROM clans WHERE clanperiod = 7;'
+mysql -se 'INSERT INTO clanpoints_history SELECT UNIX_TIMESTAMP(), id, points FROM clans WHERE period_id = 7;'
 
 # Ensure forum users are associated properly with their clans.
-mysql -se 'DELETE FROM forumuser_clans; INSERT INTO forumuser_clans SELECT phpbb3_user_group.user_id AS user_id, clans.id AS clan_id, clans.name AS clan_name FROM phpbb3_user_group INNER JOIN clans ON phpbb3_user_group.group_id = clans.forum_group_id AND clans.clanperiod = 7;'
+mysql -se 'DELETE FROM forumuser_clans; INSERT IGNORE INTO forumuser_clans SELECT phpbb3_user_group.user_id AS user_id, clans.id AS clan_id, clans.name AS clan_name FROM phpbb3_user_group INNER JOIN clans ON phpbb3_user_group.group_id = clans.forum_group_id AND clans.period_id = 7;'
 
 # Nightly backup
 dump1=/home/kgs/dbdumps/dbdump-"`date +%y%m%d-%H%M%S`".sql
